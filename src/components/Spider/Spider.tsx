40250bbb36e0005480315e33cb7d72d4c194eac9
DESKTOP-EK8DN2N\Acryl <dawson@acryl.ai>
2022-07-20 16:22:28 +0900

    [ENH] Polar -> Spider로 변경

--- src/components/Spider/Spider.tsx
+++ src/components/Spider/Spider.tsx
@@ -3,7 +3,7 @@ import make from "./make";
 import Container from "../common/Container";
 import useConfig from "hooks/useConfig";
 
-export interface PieProps {
+export interface SpiderProps {
 	data: number[][];
 	id?: string;
 	xDomain?: string[];
@@ -13,11 +13,11 @@ export interface PieProps {
 	label?: string;
 }
 
-export default function Polar({
+export default function Spider({
 	id, data, xDomain, zDomain, width="100%", height="100%", label=undefined
-}:PieProps) {
+}:SpiderProps) {
 	const group = React.useRef<HTMLDivElement>(null);
-	const config = useConfig(data, group, "Polar", id, label, xDomain, zDomain);
+	const config = useConfig(data, group, "Spider", id, label, xDomain, zDomain);
 
 	/**
 	 * config가 확정되면 차트를 생성하는 side-effect
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Line } from "./components/Line/Line";
 export { default as Bar } from "./components/Bar/Bar";
 export { default as Pie } from "./components/Pie/Pie";
-export { default as Polar } from "./components/Polar/Polar";
+export { default as Spider } from "./components/Spider/Spider";
